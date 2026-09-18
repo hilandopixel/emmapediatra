da8ebef4950d3e2bb28e3ed679468a4086a098c1
jesus <jesusgilcabezas@gmail.com>
2026-09-18 21:24:41 +0200

    mid2

--- middleware.js
+++ middleware.js
@@ -1,11 +1,9 @@
-import { NextResponse } from 'next/server.js';
+import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  // Redirige únicamente la raíz '/' hacia '/es'
   return NextResponse.redirect(new URL('/es', request.url));
 }
 
-// Configuración para que el middleware solo actúe en la ruta raíz exacta
 export const config = {
   matcher: ['/'],
 };
