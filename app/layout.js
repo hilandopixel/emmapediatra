f28d3b2884dc7aa2cef42658ca188faa9f41583e
jesus <jesusgilcabezas@gmail.com>
2026-09-22 21:14:31 +0200

    d

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,17 @@ export default async function LangLayout({ children, params }) {
   const lang = resolvedParams?.lang || "es";
   return (
     <html lang={lang}>
+      <head>
+        <script src="https://www.googletagmanager.com/gtag/js?id=G-5PY5WPDGE2" strategy="afterInteractive" />
+        <script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5PY5WPDGE2');
+          `}
+        </script>
+      </head>
       <body className="min-h-screen antialiased text-secondary-custom">
         <Navbar lang={lang} />
         {children}
